84e9e3310271bb780c6e7bf3f6976828c132ae07
Meco <mftnsy@hotmail.com>
2021-04-04 17:14:45 +0800

    spring boot Demo

---
+++ spring-demo/src/main/java/com/meco/spinrgdemo/SpinrgDemoApplication.java
@@ -0,0 +1,13 @@
+package com.meco.spinrgdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpinrgDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpinrgDemoApplication.class, args);
+	}
+
+}
